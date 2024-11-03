66572f72b783601e871d057c34ae46209e81fdf8
Ben Koch <178689076+bkoch-16@users.noreply.github.com>
2024-11-02 17:06:28 -0700

    setup parseweather function
    setup function to parse weather api response and added it to api call

--- js-file.js
+++ js-file.js
@@ -2,13 +2,26 @@ const key = "3VVAK74BCLNUZZTA5ZLDJUJHB";
 
 async function getWeather(location = "losangeles") {
   const response = await fetch(
-    ("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
-      location) &
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
+      location +
       ("?key=" + key),
     { mode: "cors" }
   );
   const weatherData = await response.json();
   console.log(weatherData);
+  const myWeather = parseWeather(weatherData);
+  console.log(myWeather);
 }
 
-getWeather();
+function parseWeather(weatherData) {
+  const myWeather = {
+    location: weatherData.resolvedAddress,
+    conditions: weatherData.currentConditions.conditions,
+    temp: weatherData.currentConditions.temp,
+    feelslike: weatherData.currentConditions.feelslike,
+    humidity: weatherData.currentConditions.humidity,
+    precip: weatherData.currentConditions.precipprob,
+    uvindex: weatherData.currentConditions.uvindex,
+  };
+  return myWeather;
+}
